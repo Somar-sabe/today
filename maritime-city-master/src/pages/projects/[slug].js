9b68d5540f75b468a244ddcec8c8296bf6ce27a1
unknown <info@altairre.ae>
2026-04-28 11:24:39 +0400

    aqewdwqwefwasdasdaddasaddsasadaasddasdsdsadasads;kadsladsadsjsea

--- maritime-city-master/src/pages/projects/[slug].js
+++ maritime-city-master/src/pages/projects/[slug].js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React, { useEffect, useMemo, useState } from "react";
 import { projects } from "../../components/ProjectsList";
 
-import HeaderSix from "@/components/header/headerSix";
+import HeaderSix from "@/components/header/HeaderSix";
 import styles from "./ProjectTimeline.module.css";
 import Footer from "@/components/footer/footer";
 import Interest from "@/components/intrest/intrest";
